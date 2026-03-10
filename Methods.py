31312b22cb3610a8fe318d72b950c9502343a4e4
markzhitnitsky <mzhitnit@student.ubc.ca>
2026-03-09 18:06:44 -0700

    Got G factor ratio fitting with uncertainty going

--- Methods.py
+++ Methods.py
@@ -1,6 +1,7 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 
 def multi_lorentz(x, A, h1, g1, x0_1, h2, g2, x0_2, h3, g3, x0_3):
@@ -67,3 +68,23 @@ def plot_fitted_transmission(time, y2_norm, popt, savename):
 
     plt.show()
     return
+
+
+def append_to_v_ratio_csv(freq, b87_u, b85_u, filename):
+    row = [
+        f"{freq}",  # Freq (assuming no error, or use freq.n)
+        "0",  # Freq_err
+        f"{b87_u.n:.8e}",  # Rb87 value
+        f"{b87_u.s:.8e}",  # Rb87 error
+        f"{b85_u.n:.8e}",  # Rb85 value
+        f"{b85_u.s:.8e}"  # Rb85 error
+    ]
+
+    file_exists = os.path.isfile(filename)
+
+    with open(filename, 'a') as f:
+        if not file_exists or os.stat(filename).st_size == 0:
+            f.write("Freq,Freq_err,Rb87,Rb87_err,Rb85,Rb85_err\n")
+            f.write("kHz,kHz,T,T,T,T\n")
+
+        f.write(",".join(row) + "\n")
